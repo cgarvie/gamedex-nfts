49c2ffe23a5d6ded6a5838343208b8323e307902
Abdul Sami <sami-abdul@outlook.com>
2018-08-16 21:21:03 +0500

    Uncommented unit tests code

--- test/token/ERC721BasicToken.test.js
+++ test/token/ERC721BasicToken.test.js
@@ -1,23 +1,23 @@
-// const { shouldBehaveLikeERC721BasicToken } = require('./ERC721BasicToken.behavior');
-// const { shouldBehaveLikeMintAndBurnERC721Token } = require('./ERC721MintBurn.behavior');
+const { shouldBehaveLikeERC721BasicToken } = require('./ERC721BasicToken.behavior');
+const { shouldBehaveLikeMintAndBurnERC721Token } = require('./ERC721MintBurn.behavior');
 
-// const BigNumber = web3.BigNumber;
-// const CardRepository = artifacts.require('CardRepository.sol');
-// const ERC721TokenMock = artifacts.require('ERC721TokenMock.sol');
+const BigNumber = web3.BigNumber;
+const CardRepository = artifacts.require('CardRepository.sol');
+const ERC721TokenMock = artifacts.require('ERC721TokenMock.sol');
 
-// const creator = web3.eth.accounts[0]
+const creator = web3.eth.accounts[0]
 
-// require('chai')
-//   .use(require('chai-bignumber')(BigNumber))
-//   .should();
+require('chai')
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
 
-// contract('ERC721TokenMock', function (accounts) {
-//   beforeEach(async function () {
-//     this.cardRepo = await CardRepository.new({ from: creator });
-//     this.token = await ERC721TokenMock.new(this.cardRepo.address, { from: creator });
-//     await this.cardRepo.transferOwnership(this.token.address, { from: creator })
-//   });
+contract('ERC721TokenMock', function (accounts) {
+  beforeEach(async function () {
+    this.cardRepo = await CardRepository.new({ from: creator });
+    this.token = await ERC721TokenMock.new(this.cardRepo.address, { from: creator });
+    await this.cardRepo.transferOwnership(this.token.address, { from: creator })
+  });
 
-//   shouldBehaveLikeERC721BasicToken(accounts);
-//   shouldBehaveLikeMintAndBurnERC721Token(accounts);
-// });
+  shouldBehaveLikeERC721BasicToken(accounts);
+  shouldBehaveLikeMintAndBurnERC721Token(accounts);
+});
