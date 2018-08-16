5efe37c2d58493bf075ecb96df9040f824afe910
Abdul Sami <sami-abdul@outlook.com>
2018-08-16 21:24:51 +0500

    Updated package.json

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,15 @@
 var DeckRepository = artifacts.require("./DeckRepository.sol");
 var CardRepository = artifacts.require("./CardRepository.sol");
 
-var GDXDeck = artifacts.require("./GDXDeck.sol");
+// var GDXDeck = artifacts.require("./GDXDeck.sol");
 var Strings = artifacts.require("./strings/Strings.sol");
 
 module.exports = function(deployer) {
     return deployer.deploy(DeckRepository).then(() => {
         return deployer.deploy(CardRepository).then(() => {
-            deployer.deploy(Strings);
-            deployer.link(Strings, GDXDeck)
-            return deployer.deploy(GDXDeck, DeckRepository.address, CardRepository.address)
+            // deployer.deploy(Strings);
+            // deployer.link(Strings, GDXDeck)
+            // return deployer.deploy(GDXDeck, DeckRepository.address, CardRepository.address)
         });
     });
 };
